598403be3fa35372aec3731fa0b9c20289d79d74
mviersel <119597405+mviersel@users.noreply.github.com>
2026-04-13 17:38:50 +0200

    add `monset` to change brightness and contrast of monitors

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -11,3 +11,27 @@ fcd() {
   file=$(fzf)
   [ -n "$file" ] && cd "$(dirname "$file")"
 }
+
+# usage:
+# monset <br_dp1> <ct_dp1> <br_dp2> <ct_dp2>
+
+monset() {
+  local br1="$1"
+  local ct1="$2"
+  local br2="$3"
+  local ct2="$4"
+
+  if [[ $# -ne 4 ]]; then
+    echo "Gebruik: monset <brightness_dp1> <contrast_dp1> <brightness_dp2> <contrast_dp2>"
+    echo "Voorbeeld: monset 70 60 50 55"
+    return 1
+  fi
+
+  # DP-2 (display 1)
+  ddcutil setvcp 10 "$br1" --display 1 --noverify
+  ddcutil setvcp 12 "$ct1" --display 1 --noverify
+
+  # DP-3 (display 2)
+  ddcutil setvcp 10 "$br2" --display 2 --noverify
+  ddcutil setvcp 12 "$ct2" --display 2 --noverify
+}
