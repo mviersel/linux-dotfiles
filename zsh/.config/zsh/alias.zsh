0690a55bc330c1c1325096028841215af80c63f6
mviersel <119597405+mviersel@users.noreply.github.com>
2026-06-08 19:24:38 +0200

    add tmux aliases

--- zsh/.config/zsh/alias.zsh
+++ zsh/.config/zsh/alias.zsh
@@ -2,5 +2,11 @@ alias win="cd /mnt/c/Users/Martijn/"
 alias la="ls -la"
 alias v="nvim"
 alias nf='nvim "$(fzf)"'
+alias v="nvim"
 
 alias lg="lazygit"
+
+alias t="tmux"
+alias ta="tmux attach"
+alias td="tmux detach"
+alias tls="tmux ls"
