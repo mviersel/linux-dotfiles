5dad235fc43016b67edc9be7d2ca452ff43f3712
mviersel <119597405+mviersel@users.noreply.github.com>
2026-06-08 19:33:40 +0200

    comment lines

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -1,10 +1,10 @@
-update() {
-if [[ -f /etc/arch-release ]]; then
-  alias update="sudo pacman -Syu"
-elif [[ -f /etc/debian_version ]]; then
-  alias update="sudo apt update && sudo apt upgrade && pipx upgrade-all"
-fi
-}
+# update() {
+# if [[ -f /etc/arch-release ]]; then
+#   alias update="sudo pacman -Syu"
+# elif [[ -f /etc/debian_version ]]; then
+#   alias update="sudo apt update && sudo apt upgrade && pipx upgrade-all"
+# fi
+# }
 
 fcd() {
   local file
