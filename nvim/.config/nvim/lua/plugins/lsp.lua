4e38d6efc32030d7c3e5877eaae98dead24aac3f
mviersel <119597405+mviersel@users.noreply.github.com>
2026-07-21 16:23:01 +0200

    add lsp's

---
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        lua_ls = {},
+        jsonls = {},
+        vtsls = {},
+        html = {},
+        cssls = {},
+        marksman = {},
+        yamlls = {},
+        bashls = {},
+        eslint = {},
+        tailwindcss = {},
+      },
+    },
+  },
+}
